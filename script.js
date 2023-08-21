bd0f1843060aa4041f7b202f04f18b1a7b1dbe1b
Muhammad Tayyab Latif <138009102+Tayyablatif19@users.noreply.github.com>
2023-08-21 16:06:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// 1-) for changing the skills under the name
+const skillsElement = document.getElementById("skills");
+const skills = ["Electrical Engineering Student", "Front-End Web Developer", "Photographer", "Digital Artist"];
+let skillIndex = 0;
+
+function updateSkills() {
+  skillsElement.textContent = skills[skillIndex];
+  skillIndex = (skillIndex + 1) % skills.length;
+}
+
+updateSkills(); 
+setInterval(updateSkills, 1500); 
+
